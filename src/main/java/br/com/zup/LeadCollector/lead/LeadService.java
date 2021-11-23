9ceece0a705cecccbaa7c5f1a0ab30dd2edcbf9f
Bárbara Romero <bqromero@gmail.com>
2021-11-23 11:48:53 -0300

    Criar método para salvar um lead

--- src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
@@ -10,38 +10,12 @@ import java.util.List;
 
 @Service
 public class LeadService {
-    @Autowired
-    private LeadRepository leadRepository;
-    @Autowired
-    private ProdutoRepository produtoRepository;
+  @Autowired
+  private LeadRepository leadRepository;
 
-    public Lead salvarLead(Lead lead) {
-        List<Produto> produtos = buscarProdutos(lead.getProdutosDeInteresse());
-        buscarProdutos(lead.getProdutosDeInteresse());
-        /*
-        O metodo tem um problema. Ele elimina os produtos já cadastrados do lead.
-        Isso não pode acontecer, faça a correção para que os produtos já cadastrado permaneçam e os novos sejam incluidos.
-         */
-        lead.setProdutosDeInteresse(produtos);
-        return leadRepository.save(lead);
-    }
+  public Lead salvarLead(Lead lead) {
+    return leadRepository.save(lead);
+  }
 
-    private List<Produto> buscarProdutos(List<Produto> produtos) {
-        List<Produto> listaAtualizada = new ArrayList<>();
-
-        for (Produto produto : produtos) {
-            if (produtoRepository.existsByNome(produto.getNome())) {
-                listaAtualizada.add(produtoRepository.findByNome(produto.getNome()));
-            }else {
-                listaAtualizada.add(produto);
-            }
-        }
-
-        return listaAtualizada;
-    }
-
-    public List<Lead> buscarTodosPeloNomeProduto(String nomeProduto){
-        return leadRepository.findAllByProdutosDeInteresseNome(nomeProduto);
-    }
 
 }
